aab584a385d9eba2b525870545e7007295ae7c35
phamnguyentruongtin <truongtin1206@gmail.com>
2023-03-23 19:56:33 +0700

    md1

---
+++ module_1/ss6_tong_quan_js/thuc_hanh/nhung_js.js
@@ -0,0 +1,3 @@
+function hienthiloichaokhachhang(){
+    alert('Xin chào!');
+}
---
+++ module_1/ss7_bien_kieudulieu_sudungbien/thuc_hanh/su_dung_toan_tu.js
@@ -0,0 +1,8 @@
+let nhapchieurong
+let nhapchieucao
+nhapchieurong = prompt('nhap chieu rong')
+nhapchieucao = prompt('nhap chieu cao')
+let chieucao = parseInt(nhapchieucao)
+let chieurong = parseInt(nhapchieurong)
+let dientich = chieurong * chieucao
+document.write('dien tich la '+ dientich)
